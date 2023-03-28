d406035c6f8e2ef16123c8cc76a19be14c75d31b
vellasamy <venkadeshl2022@gmail.com>
2023-03-28 11:07:11 +0530

    update

--- src/components/class com/Stateparent.js
+++ src/components/class com/Stateparent.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react'
-
+import StateChild from './StateChild'
 
 export class Stateparent extends Component {
-      constructor(props){
-        super(props);
-        let h=this.props.books;
-        this.state={h}
-      } 
   render() {
-    let y=this.state.h;
-     let siva=()=>{
-        let vel=y.filter((value)=> 2014 >= value.year);
-        this.setState({h:vel})
-    }
+    let  books = [
+        { name:"You Are Born to Blossom",
+        year : 2011,
+        },
+        { 
+        name:"Spirit of India",
+        year :2013,
+        },
+        { name:" Governance for Growth in India",
+        year : 2014,
+        },
+        { name:"The Family and the Nation",
+        year :2015
+        },
+        { name:"  Mission of India",
+        year :2005
+        },
+        { name:"Wings of fire",
+        year : 1999
+        },
+        { name: "You are Unique:",
+        year :2012
+        }
+        ]
     return (
-      <div>
-          <button onClick={()=>siva()}> click me</button>
-          {y.map((v)=><h1>{v.name}</h1>)}
+      <div>Stateparent
+        <StateChild books={books}/>
       </div>
     )
   }
